27992c8159fc6ade898db6ed3d62ac424fb010c7
tomvdt <tomtommy135@gmail.com>
2023-10-29 20:38:46 +0100

    rapport 3

---
+++ 03-H6-effet-hall/analyse/options.py
@@ -0,0 +1,18 @@
+rcParams = {
+    "text.usetex": True,
+    "font.family": "serif",
+    "axes.labelsize": 14,
+    "font.size": 14,
+    "legend.fontsize": 12,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
+    "figure.figsize": (4, 3),
+    "text.latex.preamble": "\n".join([
+        r"\usepackage[utf8]{inputenc}",
+        r"\usepackage[T1]{fontenc}",
+        r"\usepackage[detect-all,locale=FR]{siunitx}",
+    ]),
+    'lines.markersize': 3,
+    'lines.color': 'grey',
+    'errorbar.capsize': 3
+}
