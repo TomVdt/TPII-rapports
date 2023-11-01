d3eb5112f9c373ce50af39f367c231fdb2fb59af
tomvdt <tomtommy135@gmail.com>
2023-11-01 23:26:28 +0100

    mise en quoicoupage

--- 03-H6-effet-hall/analyse/options.py
+++ 03-H6-effet-hall/analyse/options.py
@@ -1,18 +1,18 @@
 rcParams = {
     "text.usetex": True,
     "font.family": "serif",
-    "axes.labelsize": 14,
-    "font.size": 14,
+    "axes.labelsize": 12,
+    "font.size": 12,
     "legend.fontsize": 12,
-    "xtick.labelsize": 14,
-    "ytick.labelsize": 14,
+    "xtick.labelsize": 12,
+    "ytick.labelsize": 12,
     "figure.figsize": (4, 3),
     "text.latex.preamble": "\n".join([
         r"\usepackage[utf8]{inputenc}",
         r"\usepackage[T1]{fontenc}",
         r"\usepackage[detect-all,locale=FR]{siunitx}",
     ]),
-    'lines.markersize': 10,
+    'lines.markersize': 12,
     'lines.color': 'grey',
     'scatter.marker': '+',
     'errorbar.capsize': 3
