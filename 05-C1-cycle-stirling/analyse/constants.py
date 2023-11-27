98053c8f00d59273873497129b019e0fcdac748e
tomvdt <tomtommy135@gmail.com>
2023-11-27 02:38:15 +0100

    assistant de ses morts (j'ai les cramptés)

--- 05-C1-cycle-stirling/analyse/constants.py
+++ 05-C1-cycle-stirling/analyse/constants.py
@@ -3,17 +3,22 @@ from numpy import pi
 
 # Taille du cylindre du moteur
 r_cylindre = ufloat(4, 0.1) # cm
-h_max = ufloat(7.55, 0.5) # cm
+h_max = ufloat(7.4, 0.5) # cm
 h_min = ufloat(2.04, 0.25) # cm
 V_max = pi * (r_cylindre ** 2) * h_max # cm³
 V_min = pi * (r_cylindre ** 2) * h_min # cm³
 
 # Caractéristiques du cycle
-P_max = ufloat(5.4, 0.1) # bar
-P_min = ufloat(0, 0.1) # bar
+# TODO: check values
+P_max = ufloat(0.54, 0.1) # bar
+P_min = ufloat(0.1, 0.1) # bar
 
 # Rayon du disque de freinage
 r_disque = ufloat(4, 0.001) # cm
+
+# Puissance fil
+U_fil = ufloat(13.41, 0.01) # V
+I_fil = ufloat(12.82, 0.01) # A
 
 # Temperatures
 T_eau_in = ufloat(20, 0.1) + 273 # K
@@ -26,3 +31,4 @@ T_fil = ufloat(1143, 69) # K
 debit_eau = ufloat(280, 5) # mL / min
 # https://fr.wikipedia.org/wiki/Capacit%C3%A9_thermique_massique
 C_m_eau = ufloat(4_185, 0.1) # J / K / kg
+rho_eau = ufloat(1_000, 0.001) # kg / m^3
