a27acf6d3cab8efbb45e81b9fee05f6261a14a4c
tomvdt <tomtommy135@gmail.com>
2024-03-01 18:03:07 +0100

    methode statique mah boiiiiii

--- 07-D1-torsion-elastique-des-solides/analyse/options.py
+++ 07-D1-torsion-elastique-des-solides/analyse/options.py
@@ -6,14 +6,17 @@ rcParams = {
     "legend.fontsize": 11,
     "xtick.labelsize": 12,
     "ytick.labelsize": 12,
-    "figure.figsize": (4, 3),
+    "figure.figsize": (4.5, 3.5),
     "text.latex.preamble": "\n".join([
         r"\usepackage[utf8]{inputenc}",
         r"\usepackage[T1]{fontenc}",
         r"\usepackage[detect-all,locale=FR]{siunitx}",
+        r"\usepackage{mathabx}",
     ]),
     'lines.markersize': 12,
     'lines.color': 'grey',
     'scatter.marker': '+',
-    'errorbar.capsize': 3
+    'errorbar.capsize': 3,
+    'savefig.bbox': 'tight',
+    # 'savefig.directory': '../figures/'
 }
