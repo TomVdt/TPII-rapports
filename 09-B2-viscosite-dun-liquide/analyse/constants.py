d1e341277286306c4aadfc00548b06368d93e8f8
tomvdt <tomtommy135@gmail.com>
2024-03-28 11:50:09 +0100

    anal ise

--- 09-B2-viscosite-dun-liquide/analyse/constants.py
+++ 09-B2-viscosite-dun-liquide/analyse/constants.py
@@ -6,8 +6,11 @@ delta_T = 0.5
 delta_t = 0.2
 
 # Notice me senpai
-bille_diametre = ufloat(2, 0.01) * 0.001  # m
-bille_masse = ufloat(0.00443, 3e-5)  # g
+g = 9.81  # m/s²
+k_boltzmann = 1.38 * 1e-23  # J/K
+
+bille_rayon = ufloat(1, 0.005) * 0.001  # m
+bille_masse = ufloat(0.00443, 3e-5) * 1e-3  # kg
 distance_top_bottom = ufloat(0.01, 0)  # m
 densite_1 = 819  # kg/m³
 densite_2 = 837  # kg/m³
