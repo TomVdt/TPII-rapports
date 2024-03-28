fc0451729a608a48ebbff1a6988bd80bbbf6c637
tomvdt <tomtommy135@gmail.com>
2024-03-28 11:07:32 +0100

    temple a teux

---
+++ 09-B2-viscosite-dun-liquide/analyse/constants.py
@@ -0,0 +1,15 @@
+from uncertainties import ufloat
+from numpy import pi, mean
+
+# Erreurs
+delta_T = 0.5
+delta_t = 0.2
+
+# Notice me senpai
+bille_diametre = ufloat(2, 0.01) * 0.001  # m
+bille_masse = ufloat(0.00443, 3e-5)  # g
+distance_top_bottom = ufloat(0.01, 0)  # m
+densite_1 = 819  # kg/m³
+densite_2 = 837  # kg/m³
+
+# Trucs
---
+++ 09-B2-viscosite-dun-liquide/analyse/options.py
@@ -0,0 +1,22 @@
+rcParams = {
+    "text.usetex": True,
+    "font.family": "serif",
+    "axes.labelsize": 12,
+    "font.size": 12,
+    "legend.fontsize": 12,
+    "xtick.labelsize": 12,
+    "ytick.labelsize": 12,
+    "figure.figsize": (4.5, 3.5),
+    "text.latex.preamble": "\n".join([
+        r"\usepackage[utf8]{inputenc}",
+        r"\usepackage[T1]{fontenc}",
+        r"\usepackage[detect-all,locale=FR]{siunitx}",
+        r"\usepackage{mathabx}",
+    ]),
+    'lines.markersize': 12,
+    'lines.color': 'grey',
+    'scatter.marker': '+',
+    'errorbar.capsize': 4,
+    'savefig.bbox': 'tight',
+    # 'savefig.directory': '../figures/'
+}
