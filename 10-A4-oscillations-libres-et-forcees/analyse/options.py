fff6b02b18eb00ccee1826a1e1328363d8a4ddcc
tomvdt <tomtommy135@gmail.com>
2024-04-21 17:46:39 +0200

    j'ai quasiment fini chef

--- 10-A4-oscillations-libres-et-forcees/analyse/options.py
+++ 10-A4-oscillations-libres-et-forcees/analyse/options.py
@@ -1,11 +1,11 @@
 rcParams = {
     "text.usetex": True,
     "font.family": "serif",
-    "axes.labelsize": 12,
-    "font.size": 12,
-    "legend.fontsize": 12,
-    "xtick.labelsize": 12,
-    "ytick.labelsize": 12,
+    "axes.labelsize": 14,
+    "font.size": 14,
+    "legend.fontsize": 14,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
     "figure.figsize": (4.5, 3.5),
     "text.latex.preamble": "\n".join([
         r"\usepackage[utf8]{inputenc}",
